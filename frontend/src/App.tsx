54ce5fc8979c1cd4826a9c029078c236bcb595fd
Thiago Costa <thiagomoreiracosta@gmail.com>
2021-09-10 10:31:27 -0300

    Projetct created

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import NavBar from 'components/NavBar'
+import Footer from 'components/Footer'
+import DataTable from 'components/DataTable'
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">Olá Mundo!</h1>
+
+        <DataTable />
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
